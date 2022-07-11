df179e59ff290255af463705cfe2c3cb7d9836c4
karengomar <karen.gomar30@gmail.com>
2022-07-11 12:49:56 +0200

    addition of images

--- src/components/slides/card-carrousel.component.tsx
+++ src/components/slides/card-carrousel.component.tsx
@@ -1,22 +1,25 @@
+import Grid from '@mui/material/Grid';
 import React from 'react';
 import Slides from '../../mocks/home.json';
 import CardItem from './card-item';
 
 export default function CardCarrousel() {
     return (
-       <div>
-            {Slides.slides.map((element) =>{
+        <Grid container direction="row" spacing={3}>
+            {Slides.slides.map((element) => {
                 return(
-                    element.cards.map((card) =>{
+                    element.cards.map((card) => {
+                        return(
                         <CardItem
                             id={card.id}
                             title={card.title}
                             destination={card.destination}
                             images={card.images}
                         />
+                        )
                     })
                 );
             })}
-        </div>
+       </Grid>
     );
 }
--- src/components/slides/card-item.tsx
+++ src/components/slides/card-item.tsx
@@ -1,15 +1,23 @@
-import { Box, Button, CardContent  } from '@mui/material';
+import {Button, CardContent, Grid  } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import React from 'react';
+import React, { useState } from 'react';
+
+interface CardImages {
+    desktop: string;
+    tablet: string;
+    mobile: string; 
+}
+
+type cardImagesTypes = 'desktop' | 'tablet' | 'mobile';
 
 export interface CardItemProps {
     id: number;
-    title?: string;
-    destination?: string;
-    images?:Array<{ desktop: string, tablet: string, mobile: string }>;
+    title: string;
+    destination: string;
+    images:Array<CardImages>;
 }  
 
 export default function CardItem({
@@ -18,26 +26,30 @@ export default function CardItem({
     destination,
     images,
 }: CardItemProps) {
+    const [screen, setScreen] = useState<cardImagesTypes>('desktop');
+
     return (
-        <Card sx={{ maxWidth: 345 }} key={id}>
-            <CardMedia
-                component="img"
-                height="140"
-                image="/static/images/cards/south-america.jpg"
-                alt="South America"
-            />
-            <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-                {title}
-            </Typography>
-            <Typography variant="body2" color="white">
-                {destination}
-            </Typography>
-            </CardContent>
-            <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+        <Grid item xs={4}>
+            <Card sx={{ maxWidth: 345 }} key={id}>
+                <CardMedia
+                    component="img"
+                    height='100%'
+                    image={images[0][screen]}
+                    alt={title}
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography variant="body2" color="white">
+                    {destination}
+                </Typography>
+                </CardContent>
+                <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </Grid>
     );
 }
